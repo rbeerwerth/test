61da330c6be46c70d1861c82741e188d3aeae8fc
Marina Ganeva <m.ganeva@fz-juelich.de>
2020-04-30 15:48:50 +0200

    very long line

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,5 +4,6 @@ int main(int argc, char *argv[])
 {
    std::cout << "Hello World!" << std::endl;
    std::cout << "Hello Clang-format!" << std::endl;
+   std::cout << "Very looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong lineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee." << std::endl;
    return 0;
 }
